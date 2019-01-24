6454f2e503c85d8945512fc783e37601f4ed276a
David I. Berry <dyb@noc.ac.uk>
2019-01-24 16:26:33 +0000

    missing export in NAMESPACE fixed

--- R/cds_retrieve.R
+++ R/cds_retrieve.R
@@ -1,16 +1,16 @@
 # Load required libraries
 library(httr)
 #' cds_retrieve
-#' 
+#'
 #' Function to retrieve data from the Copernicus Climate Change Service (C3S) Climate Data Store
-#' 
+#'
 #' @param dataset Dataset to extract data from.
 #' @param request JSON string containing request parameters.
 #' @param target  Filename to save data to.
 #' @param request_id Previous request ID returned by function.
 #' @return List containing request ID and name of data file downloaded, NA returned if timeout
 #' @note API Key read from ~/.cdsapirc
-#' @examples 
+#' @examples
 #' dataset <- "reanalysis-era5-single-levels"
 #' request <- "{
 #'             'variable': 'mean_sea_level_pressure',
@@ -22,6 +22,8 @@ library(httr)
 #' result <- cds_retrieve( dataset, request)
 #' # If timeout the following can be used to try again without generating new request
 #' result <- cds_retrieve( dataset, request, request_id = result$request_id)
+#'
+#' @export
 cds_retrieve <- function(dataset, request, target = '' , request_id = NA){
    # load connection details
    cdsConn <- readLines('~/.cdsapirc')
@@ -44,7 +46,7 @@ cds_retrieve <- function(dataset, request, target = '' , request_id = NA){
       if( http_error(response) ){
          print( content( response ) )
          stop()
-      } 
+      }
       request_id <- content(response)$request_id
    }else{
       source <- paste0( cdsURL, '/tasks/', request_id )
@@ -60,7 +62,7 @@ cds_retrieve <- function(dataset, request, target = '' , request_id = NA){
          response <- GET( source , authenticate(apiuid, apikey) )
          print( content( response ) )
          if( content( response )$state == 'completed' ){
-            waiting <- FALSE     
+            waiting <- FALSE
          }else if( content( response )$state %in% c('queued','running') ){
             Sys.sleep( sleeptime )
             sleeptime <- sleeptime * 1.5
@@ -78,7 +80,7 @@ cds_retrieve <- function(dataset, request, target = '' , request_id = NA){
       }else{
          outfile = target
       }
-      GET( url = content(response)$location , write_disk(outfile, overwrite = TRUE) ) 
+      GET( url = content(response)$location , write_disk(outfile, overwrite = TRUE) )
    }else{
       print( "Timeout ..." )
       outfile = NA
